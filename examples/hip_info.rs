7dfb4ecf9f63357445c0bac0f656ebe4e90e1959
d3v-null <dev.null@curtin.edu.au>
2024-05-15 03:51:23 +0000

    update hip_info demo

--- examples/hip_info.rs
+++ examples/hip_info.rs
@@ -1,6 +1,9 @@
-use hip_runtime_sys::{hipError_t, hipInit, hipDriverGetVersion, hipRuntimeGetVersion, hipGetDeviceCount, hipDeviceGetName};
+use hip_runtime_sys::{
+    hipDeviceProp_t, hipDriverGetVersion, hipError_t, hipGetDeviceCount, hipGetDeviceProperties,
+    hipInit, hipRuntimeGetVersion,
+};
 
-fn main() -> () {
+fn main() {
     let result = unsafe { hipInit(0) };
     assert_eq!(result, hipError_t::hipSuccess);
 
@@ -20,14 +23,35 @@ fn main() -> () {
     println!("Device Count: {device_count}");
 
     for i in 0..device_count {
-        let (result, name) = unsafe {
-            let mut device_name: [i8; 256] = [0; 256];
-            let result = hipDeviceGetName(device_name.as_mut_ptr(), 256, i);
-            let device_name_u8: &[u8] = unsafe { std::slice::from_raw_parts(device_name.as_ptr() as *const u8, device_name.len()) };
-            let result_str = std::str::from_utf8(&device_name_u8).unwrap();
-            (result, result_str)
+        let (name, device_prop) = unsafe {
+            let mut device_prop: hipDeviceProp_t = std::mem::zeroed();
+            let result = hipGetDeviceProperties(&mut device_prop, i);
+            assert_eq!(result, hipError_t::hipSuccess);
+            let device_name_u8: &[u8] =
+                std::slice::from_raw_parts(device_prop.name.as_ptr() as *const u8, 256);
+            let result_str = std::str::from_utf8(device_name_u8).unwrap();
+            (result_str, device_prop)
         };
-        assert_eq!(result, hipError_t::hipSuccess);
-        println!("Device {}: {}", i, name);
+        println!(
+            "Device {}: {} | multi {}",
+            i, name, device_prop.isMultiGpuBoard
+        );
+        println!(
+            " -> mem    | glb: {}GiB, shared/blk: {}KiB, ",
+            device_prop.totalGlobalMem / (1024 * 1024 * 1024),
+            device_prop.sharedMemPerBlock / (1024)
+        );
+        println!(
+            " -> thread | max/blk: {}, warpSize {}, max [{} {} {}]",
+            device_prop.maxThreadsPerBlock,
+            device_prop.warpSize,
+            device_prop.maxThreadsDim[0],
+            device_prop.maxThreadsDim[1],
+            device_prop.maxThreadsDim[2]
+        );
+        println!(
+            " -> grid   | max [{} {} {}]",
+            device_prop.maxGridSize[0], device_prop.maxGridSize[1], device_prop.maxGridSize[2]
+        );
     }
 }
